e134018e79c143e43ed2a8d184a151a93189a796
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2020-12-29 17:27:16 +0200

    Update the project for Lab03

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict'
-
+var score = 0;
 var userName = prompt("Hello fabulous person, what is your name?");
 
 while(userName === "") {
-  userName = prompt("Please ente your name");
+  userName = prompt("Please enter your name");
 }
 
 console.log(userName);
@@ -11,6 +11,7 @@ console.log(userName);
 alert("Hello " + userName +"!! ,it's pleasure to know you");
 alert("I will introduce my self by asking you some questions. please answer by yes/y or no/n");
 
+//First question
 var userInput = prompt("Do I like cats?").toLowerCase();
 
 while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInput !== "n") {
@@ -19,13 +20,14 @@ while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInpu
 }
 if(userInput === "yes" || userInput === "y") {
   alert("That's right, I mean who doesn't, they are so cute.");
+  score = score+1;
   // console.log("That's right, I mean who doesn't, they are so cute.");
 } else if (userInput === "no" || userInput === "n") {
   alert("That's not right,I love cats, now you know something about me. :)");
   // console.log("That's not right,I love cats, now you know something about me. :)");
 }
 
-
+//Second question
 userInput = prompt("my favorite color is red, is it?").toLowerCase();
 
 while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInput !== "n") {
@@ -38,9 +40,11 @@ if(userInput === "yes" || userInput === "y") {
   // console.log("That's not right, my favorite color is blue.");
 } else if (userInput === "no" || userInput === "n") {
   alert("That's right, my favorite color is blue.");
+  score = score+1;
   // console.log("That's right, my favorite color is blue.");
 }
 
+//Third question
 userInput = prompt("One of my hobbies is watching movies, is that right?").toLowerCase();
 
 while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInput !== "n") {
@@ -50,12 +54,14 @@ while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInpu
 
 if(userInput === "yes" || userInput === "y") {
   alert("That's right, I love watching comedy movies.");
+  score = score+1;
   // console.log("That's right, I love watching comedy movies.");
 } else if (userInput === "no" || userInput === "n") {
   alert("That's not right, I love watching movies.");
   // console.log("That's not right, I love watching movies.");
 }
 
+//Fourth question
 userInput = prompt("I love reading books, can you relate?").toLowerCase();
 
 while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInput !== "n") {
@@ -64,13 +70,15 @@ while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInpu
 }
 
 if(userInput === "yes" || userInput === "y") {
-  alert("That's great for you, for me I don't read but love to in the future.");
+  alert("That's great for you, for me I don't read but I love to have this hobby in the future.");
   // console.log("That's great for you, for me I don't read but love to in the future.");
 } else if (userInput === "no" || userInput === "n") {
   alert("That's fine, I also don't read books but maybe in the future.");
   // console.log("That's fine, I also don't read books but maybe in the future.");
+  score = score+1;
 }
 
+//Fifth question
 userInput = prompt("Can I swim?").toLowerCase();
 
 while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInput !== "n") {
@@ -84,19 +92,65 @@ if(userInput === "yes" || userInput === "y") {
 } else if (userInput === "no" || userInput === "n") {
   alert("you guessed right.");
   // console.log("you guessed right.");
+  score = score+1;
 }
 
-userInput = prompt("I do play video games, is that correct?").toLowerCase();
+//Sixth question
 
-while(userInput !== "yes" && userInput !== "y" && userInput !== "no" && userInput !== "n") {
-  alert("Please just asnwer by yes/y or no/n");
-  userInput = prompt("I do play video games, is that correct?").toLowerCase();
+for(let i = 1; i < 5; i++) {
+  userInput = prompt("Please guess the number between 1 to 10, you have four attempts");
+  var userNumber = parseInt(userInput);
+  //I found a source that we can check a value if it's NaN or not by checking if it's equal to itself.
+  //Source: https://stackoverflow.com/questions/2652319/how-do-you-check-that-a-number-is-nan-in-javascript
+  while(userNumber !== userNumber) {
+    userInput = prompt("Please enter a number");
+    userNumber = parseInt(userInput);
+  }
+  if(userNumber === 4) {
+    alert("You guessed it right, it's the number four :)");
+    i = 5;
+    score = score+1;
+  } else if (userNumber > 4) {
+    alert("Too high");
+  } else if (userNumber < 4) {
+    alert("Too low");
+  }
+  if(i === 4) {
+    alert("You don't have any attempts left");
+  }
 }
 
-if(userInput === "yes" || userInput === "y") {
-  alert("Yes, I play games on consoles.");
-  // console.log("Yes, I play games on consoles.");
-} else if (userInput === "no" || userInput === "n") {
-  alert("Really!?, of course I play games.");
-  // console.log("Really!?, of course I play games.");
+//Seventh question
+var movies = ["deadpool", "r.i.p.d", "the hitman's bodyguard"];
+var answer = false;
+
+alert("I love movies, please try to guess from the following movies in which movies Ryan Reynolds did act, you have six attempts");
+
+
+for(let i = 1; i < 7; i++) {
+  userInput = prompt("Deadpool, Identity Thief, Spiderman, Mama, Game Night, The Hitman's Bodyguard, R.I.P.D, Ride Along").toLowerCase();
+  for(let j = 0; j < movies.length; j++) {
+    if(userInput === movies[j]) {
+      alert("you gussed it right");
+      j = movies.length;
+      i = 7;
+      score = score+1;
+      answer = true;
+    } 
+  }
+  if(answer === false) {
+    alert("Not right, please try again");
+    if(i === 6) {
+      alert("You are out of attempts, sorry :(");
+    }
+  }
+}
+alert("The correct answers are: deadpool, r.i.p.d, the hitman's bodyguard");
+
+userInput = confirm("You did great regradless the score :), would you like to know your score?");
+
+if(userInput === true) {
+  alert("Your score is: " + score + " out of 7");
+} else {
+  alert("it's really doesn't matter");
 }
